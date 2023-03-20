82875591fcbc0f9c483e10c98fd2b9e14c8f822a
Jian Guo <jianguo@Jians-Mac-Studio.local>
2023-03-20 12:36:01 -0500

    Successfully set up express with prismic and rendered home & about pages

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
-import path from "path";
-import express from "express";
-import { fileURLToPath } from "url";
-import * as prismicH from "@prismicio/helpers";
-import { client } from "./prismicConfig.js";
-// import Prismic from "prismic-javascript";
-// const fileURLToPath = require("url").fileURLToPath;
-// const prismicH = require("@prismicio/helpers");
-// const client = require("./prismicConfig.js");
+// import path from "path";
 // import express from "express";
-// import * as path from "path";
-// const express = require("express");
-// const path = require("path");
+// import { fileURLToPath } from "url";
+// import * as prismicH from "@prismicio/helpers";
+// import { client } from "./prismicConfig.js";
+// import Prismic from "prismic-javascript";
+require("dotenv").config();
+const Prismic = require("@prismicio/client");
+const PrismicDOM = require("prismic-dom");
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.set("view engine", "pug");
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, "views")));
-
+app.set("view engine", "pug");
 // const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // app.use(express.static(path.join(__dirname, "views")));
 // Prismic API and DOM libraries for querying and rendering
-// const Prismic = require("@prismicio/client");
-// const PrismicDOM = require("prismic-dom");
 
 // Prismic API initialization
 const initApi = (req) => {
@@ -40,11 +35,11 @@ const handleLinkResolver = (doc) => {
 // Middleware
 app.use((req, res, next) => {
   res.locals.ctx = {
-    // endpoint: process.env.PRISMIC_ENDPOINT,
-    // linkResolver: handleLinkResolver,
-    prismicH,
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver,
+    // prismicH,
   };
-  // res.locals.PrismicDOM = PrismicDOM;
+  res.locals.PrismicDOM = PrismicDOM;
   next();
 });
 
@@ -61,27 +56,34 @@ app.use((req, res, next) => {
 // });
 
 // query for the root path
-app.get("/", async (req, res) => {
-  const document = await client.getFirst("");
-  res.render("home", { document });
-  // res.render("pages/home");
-});
+// app.get("/", async (req, res) => {
+//   const document = await client.getFirst("");
+//   res.render("home", { document });
+//   // res.render("pages/home");
+// });
 
 // app.get("/pages/about", async (req, res) => {
 //   const document = await client.getFirst("about");
 //   res.render("about", { document });
 // });
 
-// app.get("/about", async (req, res) => {
-//   initApi(req).then((api) => {
-//     api
-//       .query(Prismic.predicate.at("document.type", "about"))
-//       .then((response) => {
-//         console.log(response);
-//         res.render("pages/about", { document: response.results[0] });
-//       });
-//   });
-// });
+app.get("/", async (req, res) => {
+  res.render("pages/home");
+});
+
+app.get("/about", async (req, res) => {
+  initApi(req).then((api) => {
+    api
+      .query(Prismic.Predicates.at("document.type", "about"))
+      .then((response) => {
+        console.log(response);
+        res.render(
+          "pages/about"
+          // , { document: response.results[0] }
+        );
+      });
+  });
+});
 
 app.get("/detail/:uid", (req, res) => {
   res.render("pages/detail");
